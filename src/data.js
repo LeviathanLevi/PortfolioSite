1310d3ee2037ae974cdb2629c8865e34140323b0
LeviathanLevi <leutholdlevi@gmail.com>
2021-09-02 18:33:12 -0700

    Added two projects

--- src/data.js
+++ src/data.js
@@ -1,23 +1,23 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Coinbase Pro Crypto Trading Bot",
+    subtitle: "Crypto Trading Algorithm",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+      "This is a crypto trading bot made in NodeJS that uses the coinbase pro API to trade crypto. It implements various customizable trading algorithms like, momentum trading and features back testing against historical data.",
+    image: "./test.jpg",
+    link: "https://github.com/LeviathanLevi/Coinbase-Pro-Crypto-Trading-Bot-CrypFinder",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Bots and Cats website",
+    subtitle: "Pure HTML/CSS/JS website",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+      "This website explains 5 bot trading strategies, mean reversion, momentum, arbitrage, naïve bayes, and natural language processing. It was built in pure HTML/CSS and features a WebGL Unity game, interactive JS, and most importantly pictures of cats!",
+    image: "./botsandcats.gif",
+    link: "https://github.com/LeviathanLevi/BotsAndCats",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "Pyramid Crawler",
+    subtitle: "Unity Mobile Game",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-3.gif",
