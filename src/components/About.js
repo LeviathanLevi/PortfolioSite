4da6034a4cec580e108724872e3c0703d275013d
LeviathanLevi <leutholdlevi@gmail.com>
2021-09-02 17:37:01 -0700

    Added my photo and overview

--- src/components/About.js
+++ src/components/About.js
@@ -8,14 +8,11 @@ export default function About() {
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-              Hi, I'm Reed.
-              <br className="hidden lg:inline-block" />I love to build amazing
-              apps.
+              Hi, I'm Levi.
+              <br className="hidden lg:inline-block" />I'm a Software Engineer
             </h1>
             <p className="mb-8 leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-              laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-              Laborum, voluptas natus?
+            I'm 22 years old, and I was born in southern Oregon. Growing up I always has a passion for technology, whether it was computer hardware, 3D animation, virtual reality, robotics, game development, or programming. I knew that I wanted to work with computers, so in highschool when I went to community college for a dual enrollment program, I started studying software engineering. I later transfered to Oregon Tech where I graduated with my bachelor’s in software engineering technology. Currently, I'm finishing up my 2nd internship to conclude the MECOP program.
             </p>
             <div className="flex justify-center">
               <a
@@ -23,18 +20,13 @@ export default function About() {
                 className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
                 Work With Me
               </a>
-              <a
-                href="#projects"
-                className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                See My Past Work
-              </a>
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
             <img
               className="object-cover object-center rounded"
-              alt="hero"
-              src="./coding.svg"
+              alt="Picture of me"
+              src="./pictureOfMe.png"
             />
           </div>
         </div>
