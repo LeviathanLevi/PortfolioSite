35901b46d999ac2d0049a8b0e47482bcfc0c4553
LeviathanLevi <leutholdlevi@gmail.com>
2021-09-10 09:51:55 -0700

    Updated resume to PDF

--- src/components/About.js
+++ src/components/About.js
@@ -21,8 +21,8 @@ export default function About() {
                 Work With Me
               </a>
               <a
-                href="./Resume.docx"
-                download="Resume.docx"
+                href="./Resume.pdf"
+                download="Resume.pdf"
                 className="ml-4 inline-flex text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
                 Resume
               </a>
