7889fcd76eb430e875820adf49f718a53a110f0a
LeviathanLevi <leutholdlevi@gmail.com>
2021-09-13 10:08:37 -0700

    Updated decscriptions, skills, and resume.

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Coinbase Pro Crypto Trading Bot",
     subtitle: "Crypto Trading Algorithm",
     description:
-      "This is a crypto trading bot made in NodeJS that uses the coinbase pro API to trade crypto. It implements various customizable trading algorithms like, momentum trading and features back testing against historical data.",
+      "Crypto trading bot made in NodeJS that uses the Coinbase Pro API and WebSocket to implement different trading strategies such as momentum, reverse, and stop loss trading. Implemented back testing functionalitythat uses historical OHLC data to analyze trading strategies.",
     image: "./coinbasero.png",
     link: "https://github.com/LeviathanLevi/Coinbase-Pro-Crypto-Trading-Bot-CrypFinder",
   },
@@ -19,7 +19,7 @@ export const projects = [
     title: "Pyramid Crawler",
     subtitle: "Unity Mobile Game",
     description:
-      "Pyramid Crawler is a Unity game for Android/IOS. It's a turn-based game that utilizes RNG mechanics and plays on a grid where the player moves to defeat enemies and advance through stages. It features 3D models and assets I created in Blender3D and C# scripting.",
+      "A turn based mobile game developed in Unity and Blender that plays on a grid and uses random number generated game mechanics. I developed all the 3D assets, software, and processes.",
     image: "./pyramidcrawler.gif",
     link: "https://github.com/LeviathanLevi/PyramidCrawler",
   },
@@ -27,7 +27,7 @@ export const projects = [
     title: "Trader Invader",
     subtitle: "React/NodeJS Group Project",
     description:
-      "Trader Invader was a group project where me and 3 others made a stock trading simulation game. It uses React for the frontend and NodeJS for the backend and used DynamoDB for storing data. While it's no longer hosted on AWS you can find the project files here.",
+      "Team-based project to design and develop a web application that lets players compete in stock trading competitions. My role focused on requirements writing, design, and backend development. Tech stack included: React, S3, Cognito, DynamoDB, and NodeJS-based AWS Lambdas.",
     image: "./traderinvader.png",
     link: "https://github.com/TraderInvaderJP",
   },
@@ -36,14 +36,14 @@ export const projects = [
 export const experiences = [
   {
     quote:
-      "Worked on a comprehensive data and analytics application for added value reporting. The app was designed with a microservices architecture in mind and built using NodeJS, Java, React, and AWS. I assisted in design/development and implemented backend solutions that added features.",
+      "Implemented and designed solutions for a comprehensive data and analytics application for added value reporting built with a microservices architecture. Focused on backend development and built features such as prescription savings and medication supply reporting. Implemented with NodeJS, React, Ruby, Docker, CircleCI, Cucumber, and AWS.",
     image: "./cambia.jpg",
     companyName: "Cambia Health Solutions",
     date: "Jun. 2020 - Mar. 2021 (9 months)",
   },
   {
     quote:
-      "Working with the aftermarket team to maintain and develop applications supporting trucks operating out in the world. Developed an E2E testing suite and fixed user reported bugs for an application that presents information on individual trucks.",
+      "Developed an end-to-end testing suite and implemented solutions to user reported bugs to support an application that presents information on individual trucks. Supported the aftermarket team to maintain and develop applications for commercial trucks.",
     image: "./daimler.jpeg",
     companyName: "Daimler Trucks North America",
     date: "Jun. 2021 - present",
@@ -65,4 +65,4 @@ export const educations = [
   },
 ];
 
-export const skills = ["NodeJS","C","C++","C#",".NET","JavaScript","Ruby","HTML","CSS","AWS","Lambda","S3","ECS Tasks","Docker","Cucumber/Gherkin","Nightwatch.js","Mocha/Chai","SQL Server","Big Query","Concurrent Programming","Jira","Git","Unity Game Engine","Blender3D"];
+export const skills = ["NodeJS","C","C++","C#",".NET","JavaScript","Ruby","HTML","CSS","AWS","Lambda","S3","ECS Tasks","Docker","Cucumber/Gherkin","Agile Team Planning","Jenkins","SQL Server","UI Testing","Concurrent Programming","Jira","Git","Unity Game Engine","Blender3D"];
