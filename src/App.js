b4154ef94774053643eaf99415a2b35420e3a864
LeviathanLevi <leutholdlevi@gmail.com>
2021-09-13 09:56:46 -0700

    Removed GA and updated bios

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
-import ReactGA from 'react-ga';
-
-const TRACKING_ID = "UA-207411549-1"; 
-ReactGA.initialize(TRACKING_ID);
 
 export default function App() {
   return (
